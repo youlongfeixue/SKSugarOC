1bcc6718a057108408f9096755f1c948fbae585d
高昌绪 <gcx01293704@alibaba-inc.com>
2022-03-02 14:31:45 +0800

    添加UIButton+SKAddition

---
+++ Classes/SKAddition/UIButton+SKAddition.h
@@ -0,0 +1,17 @@
+//
+//  UIButton+SKAddition.h
+//  SKSugarOCExample
+//
+//  Created by Sky-work on 2022/3/2.
+//  Copyright © 2022 YouLong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface UIButton (SKAddition)
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Classes/SKAddition/UIButton+SKAddition.m
@@ -0,0 +1,13 @@
+//
+//  UIButton+SKAddition.m
+//  SKSugarOCExample
+//
+//  Created by Sky-work on 2022/3/2.
+//  Copyright © 2022 YouLong. All rights reserved.
+//
+
+#import "UIButton+SKAddition.h"
+
+@implementation UIButton (SKAddition)
+
+@end
